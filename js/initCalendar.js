e8a5c0897d1d2058a66855ba82520050b0cf82fa
Connor Lantz <cal0064@auburn.edu>
2021-02-22 16:29:05 -0600

    Removed old classes from main page, moved calendar code to it's own file

---
+++ js/initCalendar.js
@@ -0,0 +1,21 @@
+// Refer to FullCalendar docs for more customization
+// API key: AIzaSyA3uX2Qdh5m67ho8ELTdTtdwLENIZ_0SIM
+// Address: autigerdev@gmail.com
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      aspectRatio: 5,
+      height: 800,
+      googleCalendarApiKey: 'AIzaSyA3uX2Qdh5m67ho8ELTdTtdwLENIZ_0SIM',
+      eventSources: [
+        {
+          googleCalendarId: 'autigerdev@gmail.com'
+        }, {
+          googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com'
+        }
+      ]
+    });
+  
+    calendar.render();
+  });
