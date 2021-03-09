038b2685fe3a82e20c8e7e594a69ca929e533bc1
Harper Rhett <harperrhett@gmail.com>
2021-03-09 16:58:58 -0600

    Calendar fix for mobile
    This is the best it's going to be for the time being

--- js/initCalendar.js
+++ js/initCalendar.js
@@ -1,11 +1,22 @@
 // Refer to FullCalendar docs for more customization
 // API key: AIzaSyA3uX2Qdh5m67ho8ELTdTtdwLENIZ_0SIM
 // Address: autigerdev@gmail.com
+
+// Initialize
+var ratio = 2.5;
+var view = 'dayGridMonth';
+
+// Change ratio for mobile
+if (screen.width <= 600) {
+  ratio = 0.75;
+}
+
+// Create calendar
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      aspectRatio: 2.5,
+      initialView: view,
+      aspectRatio: ratio,
       googleCalendarApiKey: 'AIzaSyA3uX2Qdh5m67ho8ELTdTtdwLENIZ_0SIM',
       eventSources: [
         {
